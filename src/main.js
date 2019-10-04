14682233e5f068245b4be92043792979de175bf4
赖琳环 <15994920424@163.com>
2019-10-04 15:13:18 +0800

    修改了README.md和main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 //导入App根组件
 import app from './App.vue'
-//按需导入Mint-UI中的组件
+//按需导入Mint-UI中的组件     
 import { Header } from 'mint-ui'
 Vue.component(Header.name,Header)
 //导入MUI的样式
